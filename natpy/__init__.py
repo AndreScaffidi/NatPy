b6444c594b0e418198898dfe1550bac21e2bdca2
Tomas Liam Howson <tomas.howson@adelaide.edu.au>
2020-09-04 11:37:48 +0930

    Access units through natpy, convert as method

--- natpy/__init__.py
+++ natpy/__init__.py
@@ -4,7 +4,10 @@ from .convert import convert
 from .default_values import default_conventions
 from .default_values import set_active_units, list_active_units, get_active_units
 from .constants import *
-from astropy.constants import * 
+from astropy import constants as const 
+from astropy.units.core import UnitBase
+from astropy.units import *
 
+UnitBase.convert = convert
 
 set_active_units("hbar_c")
